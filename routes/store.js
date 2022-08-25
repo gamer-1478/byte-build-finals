4bf3ea365cafd36844f3e11d3c625317f9fa3708
AlightKnight343 <thegodofgreatness343@gmail.com>
2022-08-25 09:48:58 +0530

    Search letsgo

--- routes/store.js
+++ routes/store.js
@@ -1,13 +1,14 @@
 const express = require('express'),
     router = express.Router(),
     {store_admin, store_admin_create, store_admin_get_product,store_admin_post_product, store_admin_delete_product, store, store_item_view, store_item_buy, store_search}  = require('../controllers/store.js'),
-    {ensureAdminAuthenticated} = require('../middlewares/authenticate.js')
+    {ensureAdminAuthenticated, ensureAuthenticated} = require('../middlewares/authenticate.js')
 
 //store customer routes
-router.get('/', store)
-router.get('/view/:id', store_item_view)
-router.post('/buy/:id', store_item_buy)
-router.post('/search', ensureAuthenticated, store_search)
+router.get('/', ensureAuthenticated, store)
+router.get('/view/:id', ensureAuthenticated, store_item_view)
+router.post('/buy/:id', ensureAuthenticated, store_item_buy)
+router.get('/search', ensureAuthenticated, store_search)
+
 
 //store admin routes
 router.get('/admin', ensureAdminAuthenticated, store_admin)
